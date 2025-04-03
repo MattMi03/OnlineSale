28386589b5bc0f5003bae205fc4a055f3989d428
Matt Mi <yizhangmi03@gmail.com>
2025-04-03 16:57:59 +0800

    初始化

---
+++ coupon/src/test/java/com/matt/onlinesale/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matt.onlinesale.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
